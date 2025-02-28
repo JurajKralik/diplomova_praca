f7080722df420eb0ede58c3f9dfd79faf5938220
JurajKralik <juraj.kralik95@gmail.com>
2025-02-28 11:35:10 +0100

    python SpeechRecognition

---
+++ testing/testing_speech_recognition.py
@@ -0,0 +1,25 @@
+import speech_recognition
+
+
+while True:
+    print("Listening...")
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer = speech_recognition.Recognizer()
+
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            if text == "stop":
+                print("Stopped")
+                break
+
+            print(text)
+
+    except speech_recognition.UnknownValueError():
+
+        recognizer = speech_recognition.Recognizer()
+        continue
