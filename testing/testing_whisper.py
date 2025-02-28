defa2b03abf3abd19b11a9170af939fa5993a5b9
JurajKralik <juraj.kralik95@gmail.com>
2025-02-28 14:13:57 +0100

    whisper works

--- testing/testing_whisper.py
+++ testing/testing_whisper.py
@@ -1,1 +1,7 @@
 import whisper
+
+
+file_name = 'C:\\Users\\DAX\\Desktop\\School\\_Diplomová práca\\testing\\test_short.wav'
+model = whisper.load_model("turbo")
+result = model.transcribe(file_name)
+print(result["text"])
